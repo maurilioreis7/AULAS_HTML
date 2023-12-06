c567ea9d113c5e6accacb594c66b634584b6b565
maurilioreis7 <142803887+maurilioreis7@users.noreply.github.com>
2023-12-06 11:00:04 -0300

    Add files via upload
    adicionei aulas

---
+++ JAVA_SCRIPT/estrutura de controle.js
@@ -0,0 +1,12 @@
+// ESTRUTURA DE CONTROLE
+
+//Exercício 1: criar um programa que verifica se um número é par ou ímpar.
+
+let numero = 8;
+if (numero % 2 === 0) {
+    console.log("O númeor par.");
+} else {
+    console.log("O número é ímpar.");
+}
+
+//Exercício 2: Escrever um programa que verifica se um aluno foi aprovado ou reprovado com base em sua média.
